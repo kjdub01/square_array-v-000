1dde2813f65f7f875f5b12c7dc6f720cbedaca8f
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-07-29 01:54:41 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,12 @@
+require pry
+
 def square_array(array)
   arr = []
   array.each{|i| arr << i ** 2}
   arr
 end
 
+def square_array_with_collect(array)
+  array.collect{|i| i ** 2}
+end
+binding.pry
